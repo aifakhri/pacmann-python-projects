52b27aa3dbf8cfcd2959df94e936d52d1ac2221f
fakhri <fakhri.azhar@proton.me>
2022-11-10 18:56:27 +0700

    Modify some function

--- script.py
+++ script.py
@@ -1,7 +1,14 @@
+import pandas as pd
+
+
+
+
+
 class Transaction:
     """
     This is the feature to add, update, delete and calculate user shopping cart
     """
+    COLUMN_NAME = ["Nama Item", "Jumlah Item", "Harga/Item", "Total Harga"]
     LOWER_AMOUNT = 200_000
     LOWER_DISCOUNT = 0.05
     MID_AMOUNT = 300_000
@@ -11,119 +18,112 @@ class Transaction:
 
 
     def __init__(self):
-        """
-        Instance Attributes to store item information that is submitted by user
-        """
-        self._cart = []
-
-    def add_item(self, name: str, qty: int, price: float):
-        """
-        Method that is used to store the details of information
-        of the item that is submitted by user into user cart.
-        """
-        self._items_info = {
+        self.cart = []
+    
+    def add_item(self, name: str, qty=0, price=0.0):
+        self.items_info = {
             "name": name,
             "qty": qty,
             "price": price,
             "amount": price*qty
         }
-        self._cart.append(self._items_info)
+        self.cart.append(self._items_info)
 
-    def update_item_name(self, name: str, newName: str):
-        """
-        Method that is used to update item name in the current cart.
-        """
-        for key in self._cart:
-            if key["name"] == name:
-                key["name"] = newName
+    def update_item_name(self, name: str, new_name: str):
+        item_location = self.find_item(name)
 
-    def update_item_qty(self, name: str, newQty: int):
-        """
-        Method that is used to update item quantity in the current cart.
-        Each update would also update the amount.
-        """
-        for key in self._cart:
-            if key["name"] == name:
-                key["qty"] = newQty
-                key["amount"] = key["price"]*newQty
+        if (item_location != None):
+            self.cart[item_location]["name"] = new_name
+        else:
+            print("Item Tidak Ditemukan, Mohon Cek Kembali Keranjang Anda")        
 
-    def update_item_price(self, name: str, newPrice: float):
-        """
-        Method that is used to update item price in the current cart.
-        Each update would also update the amount.
-        """
-        for key in self._cart:
-            if key["name"] == name:
-                key["price"] = newPrice
-                key["amount"] = key["qty"]*newPrice
+    def update_item_qty(self, name: str, new_qty: int):
+        item_location = self.find_item(name)
+
+        if (item_location != None):
+            self.cart[item_location]["qty"] = new_qty
+            self.cart[item_location]["amount"] = self.cart[item_location]["price"]*new_qty  
+        else:
+            print("Item Tidak Ditemukan, Mohon Cek Kembali Keranjang Anda")
+
+    def update_item_price(self, name: str, new_price: float):
+        item_location = self.find_item(name)
+
+        if (item_location != None):
+            self.cart[item_location]["price"] = new_price
+            self.cart[item_location]["amount"] = self.cart[item_location]["price"]*new_price
+        else:
+            print("Item Tidak Ditemukan, Mohon Cek Kembali Keranjang Anda")
+        
 
     def delete_item(self, name: str):
-        """
-        Method that is used to delete one item from the cart.
-        """
-        self._cart = [i for i in self._cart if not i["name"] == name]
+        item_location = self.find_item(name)
+
+        if (item_location != None):
+            self.cart = [i for i in self.cart if not i["name"] == name]
+        else:
+            print("Item Tidak Ditemukan, Mohon Cek Kembali Keranjang Anda")
 
     def reset_transaction(self):
-        """
-        Method for delete the entire item on the cart.
-        """
-        self._cart = []
+        self.cart = []
 
     def check_order(self):
-        """
-        Method to validate the entry of the cart. And also shows the user
-        the information about the current cart. 
-        """
-        header = "| No | Name Item | Harga/Item | Total Harga |\n"
-        for i, elem in enumerate(self._cart, start=1):
-            if (elem["name"] == "") or (elem["qty"] == "") or (elem["price"] == ""):
-                print("Invalid Input Data")
-                exit()
+        if self.cart:
+            return "Anda Tidak Memiliki Barang di Keranjang"
+        
+        count = 0
+        for item in self.cart:
+            if (item["name"] == "") or (item["qty"] == 0) or (item["price"] == 0.0):
+                count += 1
             else:
-                content = f"| {i} | {elem['name']} | {elem['qty']} | {elem['price']}, | {elem['amount']} |\n"
-                header += content
-        print(header)
+                continue
+        
+        df = pd.DataFrame(self.cart)
+        df.columns = self.COLUMN_NAME
+        
+        if (count == 0):     
+            print("Pemesanan Sudah Sesuai")
+        elif (count > 0):
+            print("TERDAPAT KESALAHAN INPUT")
+        
+        print(df)
+        
 
     def total_price(self):
-        """
-        Calculate the total amount of the item price in the cart.
-        This method also calculcate the discount if the total amount reaches
-        a certain number
-        """
         total = 0
-        for elem in self._cart:
+        for elem in self.cart:
             total += elem["amount"]
-
-        print("Total amount of your shopping cart is ", total)
         
         if (total > self.HIGH_AMOUNT):
             total = total - (total*self.HIGH_DISCOUNT)
-            print("Congratulation You Get a discount, your total shopping amount is ", total)
+            print("SELAMAT ANDA MENDAPATKAN DISKON 20%!, Total Belanja Anda Adalah: ", total)
         elif (total > self.MID_AMOUNT) and (total < self.HIGH_AMOUNT):
             total = total - (total*self.MID_DISCOUNT)
-            print("Congratulation You Get a discount, your total shopping amount is ", total)
+            print("SELAMAT ANDA MENDAPATKAN DISKON 20%!, Total Belanja Anda Adalah: ", total)
         elif (total > self.LOWER_AMOUNT) and (total < self.MID_AMOUNT):
             total = total - (total*self.LOWER_AMOUNT)
-            print("Congratulation You Get a discount, your total shopping amount is ", total)
-        
-### Another things to do: Add data validation on total_price() method.
-                
+            print("SELAMAT ANDA MENDAPATKAN DISKON 20%!, Total Belanja Anda Adalah: ", total)
+        else:
+            print("Total Belanja Anda Adalah: ", total)
+
+    # Additional Functions That Is Not on The Project Requirement
+
+    def find_item(self, item_name):
+        # print(item_name)
+        for i, item in enumerate(self.cart):
+            if item["name"] == item_name:
+                return i
 
 if __name__ == "__main__":
     test = Transaction()
     test.add_item("test", 1, 100_000)
     test.add_item("test2", 2, 2000)
-    test.add_item("test3", 3, 3000)
-    test.add_item("test4", 4, 4000)
+
+    test.check_order()
+
+    test.update_item_name("test3", "test10")
+    test.update_item_price("test3", 1000)
+    test.update_item_qty("test3", 10)
+    # test.add_item("")
     test.check_order()
     test.total_price()
-    # print("Test modify chart")
-    # test.update_item_name("test2", "testUpdate")
-    # test.update_item_qty("test3", 100 )
-    # test.update_item_price("test4", 1000)
-    # test.check_order()
-    # print("Test Delete")
-    # test.delete_item("test")
-    # test.check_order()
-    # test.reset_transaction()
-    # test.check_order()
